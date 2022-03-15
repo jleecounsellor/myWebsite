fc18acab06d0b6d0d1303a40a7e6a6043798779b
jleecounsellor <jleecounsellor@gmail.com>
2022-03-14 19:31:24 -0700

    update port

--- app.js
+++ app.js
@@ -59,10 +59,8 @@ app.post("/contact", function(req, res){
   });
 });
 
-let port = process.env.PORT;
-if (port == null || port = "") {
-  port = 3000;
-}
+let port = process.env.PORT || 3000;
+
 
 app.listen(port, function() {
   console.log("Server started on port " + port);
